f1bebc879355575a7b2c4e5f38a5c694ce2317b8
masonicgryphon7 <thejwalkthrough@gmail.com>
2016-06-30 18:06:14 +0200

    Fixed A Bit
    Added Folders...

---
+++ FP_Horror Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
